c560267f716214efc9697b7143cb91f2d6e0c42e
abrusut <andresbrusutti@gmail.com>
2019-09-27 14:48:33 -0300

    evaluation controller

--- API-test/app/Http/Controllers/API/EvaluationController.php
+++ API-test/app/Http/Controllers/API/EvaluationController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Evaluations;
 use App\Http\Controllers\Controller;
+use App\User;
+use App\UserType;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class EvaluationController extends Controller
 {
@@ -12,49 +16,75 @@ class EvaluationController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $evaluations = Evaluations::all();
+        return response()->json($evaluations, 200);
     }
-
+    
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
+     * Display a listing of the resource.
+     * @param int $page
+     * @param int $size
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function getAllPaginate(Request $request, $size)
     {
-        //
+        $evaluations = DB::table('evaluations')->paginate($size);
+        
+        return response()->json($evaluations, 200);
     }
-
+    
+    
     /**
-     * Display the specified resource.
+     * Display a listing of the resource.
      *
-     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        //
+        return response()->json(Evaluations::where('id', $id)->get());
     }
-
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $teacher = $request->teacher;
+        $student = $request->student;
+        
+        $evaluation = new Evaluations();
+        $evaluation->score = $request->score;
+        $evaluation->students()->save($student);
+        $evaluation->teachers()->save($teacher);
+    
+        $evaluation->save();
+        return response()->json($evaluation, 201);
+    }
+    
+    
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Evaluations $evaluations)
     {
-        //
+        $user = [];
+        return response()->json($user, 200);
+        
     }
-
+    
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
--- API-test/app/User.php
+++ API-test/app/User.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     protected $fillable = ['name', 'email', 'password', 'role'];
+    
+    public function students()
+    {
+        return $this->hasMany('User', 'student_id');
+    }
+    
+    public function teachers()
+    {
+        return $this->hasMany(User::class, 'teacher_id');
+    }
 }
